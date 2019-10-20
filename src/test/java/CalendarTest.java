2b470b7fea217a867962423c9b637a4d7b342e74
Mateusz Tadla <mtadla@sii.pl>
2019-10-20 16:49:27 +0200

    first change

--- src/test/java/CalendarTest.java
+++ src/test/java/CalendarTest.java
@@ -7,7 +7,8 @@ import java.util.Arrays;
 import java.util.List;
 
 public class CalendarTest extends TestBase {
-    List<String> allMonths = Arrays.asList("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
+    List<String> allMonths =
+            Arrays.asList("January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December");
 
     @Test
     public void Test() {
