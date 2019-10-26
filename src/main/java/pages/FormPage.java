b37663a3db1afbe9d005ef8fef25ce83d6381504
Mateusz Tadla <mtadla@sii.pl>
2019-10-26 14:11:42 +0200

    add rbt

--- src/main/java/pages/FormPage.java
+++ src/main/java/pages/FormPage.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
+import java.util.List;
+
 public class FormPage {
     public FormPage(WebDriver driver) {
         PageFactory.initElements(driver, this);
@@ -13,6 +15,9 @@ public class FormPage {
 
     @FindBy(id = "inputFirstName3")
     private WebElement firstName;
+
+    @FindBy(name = "gridRadiosSex")
+    private List<WebElement> sexRadioButtons;
 
     public void setFirstName(String name) {
         firstName.sendKeys(name);
