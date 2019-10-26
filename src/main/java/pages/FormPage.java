36792c9de03b33d42195ffdd40cb7ae34de3c5b0
Mateusz Tadla <mtadla@sii.pl>
2019-10-26 15:39:46 +0200

    add po for form test

--- src/main/java/pages/FormPage.java
+++ src/main/java/pages/FormPage.java
@@ -4,8 +4,10 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
 
 import java.util.List;
+import java.util.Random;
 
 public class FormPage {
     public FormPage(WebDriver driver) {
@@ -16,10 +18,107 @@ public class FormPage {
     @FindBy(id = "inputFirstName3")
     private WebElement firstName;
 
+    @FindBy(id = "inputLastName3")
+    private WebElement lastName;
+
+    @FindBy(id = "inputEmail3")
+    private WebElement email;
+
     @FindBy(name = "gridRadiosSex")
     private List<WebElement> sexRadioButtons;
 
-    public void setFirstName(String name) {
-        firstName.sendKeys(name);
+    @FindBy(id = "inputAge3")
+    private WebElement age;
+
+    @FindBy(name = "gridRadiosExperience")
+    private List<WebElement> yearsOfExperience;
+
+    @FindBy(id = "gridCheckManulTester")
+    private WebElement manualTesterProfession;
+
+    @FindBy(id = "selectContinents")
+    private WebElement continents;
+
+    @FindBy(id = "selectSeleniumCommands")
+    private WebElement seleniumCommands;
+
+    @FindBy(id = "chooseFile")
+    private WebElement fileInput;
+
+    @FindBy(css = "button[type='submit']")
+    private WebElement submit;
+
+    @FindBy(id = "validator-message")
+    private WebElement message;
+
+    public void setFirstName(String firstName) {
+        this.firstName.sendKeys(firstName);
     }
+
+    public void setLastName(String lastName) {
+        this.lastName.sendKeys(lastName);
+    }
+
+    public void setEmail(String email) {
+        this.email.sendKeys(email);
+    }
+
+    public void selectSex(int index) {
+        sexRadioButtons.get(index).click();
+    }
+
+    public void selectSex(String sexName) {
+        for (WebElement sex : sexRadioButtons) {
+            if (sex.getAttribute("value").equals(sexName)) {
+                sex.click();
+            }
+        }
+    }
+
+
+    public void setAge(String age) {
+        this.age.sendKeys(age);
+    }
+
+    public void randomYearsOfExperience() {
+        getRandomElement(yearsOfExperience).click();
+    }
+
+    public void selectManualTesterProfession() {
+        manualTesterProfession.click();
+    }
+
+    public void selectRandomContinent() {
+        Select continentsSelect = new Select(continents);
+        WebElement randomOption = getRandomElement(continentsSelect.getOptions());
+        selectContinent(randomOption.getText());
+    }
+
+    public void selectContinent(String continent) {
+        new Select(continents).selectByVisibleText(continent);
+    }
+
+    public void selectSeleniumCommand(String command) {
+        new Select(seleniumCommands).selectByValue(command);
+    }
+
+    public void addFile(String filePath) {
+        fileInput.sendKeys(filePath);
+    }
+
+
+    public void submit() {
+        submit.click();
+    }
+
+    public String getMessage() {
+        return message.getText();
+    }
+
+    private WebElement getRandomElement(List<WebElement> elements) {
+        Random rnd = new Random();
+        int randomNumber = rnd.nextInt(elements.size() - 1);
+        return elements.get(randomNumber);
+    }
+
 }
--- src/test/java/FormPoTest.java
+++ src/test/java/FormPoTest.java
@@ -1,12 +1,27 @@
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import pages.FormPage;
+
+import java.io.File;
 
 public class FormPoTest extends TestBase {
 
     @Test
     public void newFormTest(){
+        File file = new File("src\\main\\resources\\emptyFile.txt");
         driver.get("http://seleniumui.tc-sii.com/form.php");
         FormPage formPage = new FormPage(driver);
         formPage.setFirstName("jan");
+        formPage.setLastName("kowalski");
+        formPage.setEmail("jan@wp.pl");
+        formPage.selectSex("male");
+        formPage.setAge("15");
+        formPage.randomYearsOfExperience();
+        formPage.selectManualTesterProfession();
+        formPage.selectRandomContinent();
+        formPage.selectSeleniumCommand("switch-commands");
+        formPage.addFile(file.getAbsolutePath());
+        formPage.submit();
+        Assert.assertEquals(formPage.getMessage(), "Form send with success");
     }
 }
